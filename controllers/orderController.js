34ad5d266590f22c60b276cffe98c809ec5ff7b5
bimocahyo7 <bimochayo@gmail.com>
2023-12-26 13:01:12 +0700

    Add attribute User in orderController

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -18,6 +18,7 @@ class Controller {
               },
               {
                 model: User,
+                attributes: ["id", "nama", "alamat"],
               },
             ],
           },
